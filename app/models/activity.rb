67a3051404f856c4f6f4ee2ff335508a450b1ebb
josereyessaldias <jpreyess@uc.cl>
2019-04-28 20:52:52 -0400

    rollbanck migration

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,7 +5,7 @@ class Activity < ApplicationRecord
 	belongs_to :category, optional: true
 	has_many :promote_activity
 
-	belongs_to :owner, class_name: 'User'
+	belongs_to :user, class_name: 'User', foreign_key: :owner
 
 	mount_uploader :photo, ImageUploader
 end
