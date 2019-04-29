53a95d8ee56e8193c1c47844a90a855b25a263f5
josereyessaldias <jpreyess@uc.cl>
2019-04-28 20:43:46 -0400

    actualicé el seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,5 @@ User.create(name:"Ganso",email:"ganso@ganso.ganso",password:'123456')
 User.create(name:"Gato",email:"gato@gato.gato",password:'123456')
 User.create(name:"Oso",email:"oso@oso.oso",password:'123456')
 
-Activity.create(name:'The Princess Diaries',category_id:2,remote_photo_url:'https://upload.wikimedia.org/wikipedia/en/thumb/5/58/Princess_diaries_ver1.jpg/220px-Princess_diaries_ver1.jpg')
-Activity.create(name:'Matrix',category_id:2,remote_photo_url:'https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg')
+Activity.create(name:'The Princess Diaries',owner_id:1,category_id:2,remote_photo_url:'https://upload.wikimedia.org/wikipedia/en/thumb/5/58/Princess_diaries_ver1.jpg/220px-Princess_diaries_ver1.jpg')
+Activity.create(name:'Matrix',owner_id:1,category_id:2,remote_photo_url:'https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg')
